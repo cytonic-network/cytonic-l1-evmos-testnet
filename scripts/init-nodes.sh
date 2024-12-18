5e7644b447740cf4dbce2d9076915370d49a6535
ChronosXYZ <chronosxyz000@gmail.com>
2024-12-18 20:47:54 +0300

    Add printing mnemonics

--- scripts/init-nodes.sh
+++ scripts/init-nodes.sh
@@ -12,15 +12,22 @@ docker compose run --no-deps --rm node-d init node-d --chain-id $CHAIN_ID --home
 
 cp genesis.json data/chain/node-a/config
 
-NODE_A_KEY=$(docker compose run --no-deps --rm node-a keys add validator-a --keyring-backend test --home $CHAIN_HOME/node-a --output json --no-backup | jq -r .address)
-NODE_B_KEY=$(docker compose run --no-deps --rm node-b keys add validator-b --keyring-backend test --home $CHAIN_HOME/node-b --output json --no-backup | jq -r .address)
-NODE_C_KEY=$(docker compose run --no-deps --rm node-c keys add validator-c --keyring-backend test --home $CHAIN_HOME/node-c --output json --no-backup | jq -r .address)
-NODE_D_KEY=$(docker compose run --no-deps --rm node-d keys add validator-d --keyring-backend test --home $CHAIN_HOME/node-d --output json --no-backup | jq -r .address)
 
-docker compose run --no-deps --rm node-a add-genesis-account $NODE_A_KEY 10000000000000000000000000000aevmos --home $CHAIN_HOME/node-a
-docker compose run --no-deps --rm node-a add-genesis-account $NODE_B_KEY 10000000000000000000000000000aevmos --home $CHAIN_HOME/node-a
-docker compose run --no-deps --rm node-a add-genesis-account $NODE_C_KEY 10000000000000000000000000000aevmos --home $CHAIN_HOME/node-a
-docker compose run --no-deps --rm node-a add-genesis-account $NODE_D_KEY 10000000000000000000000000000aevmos --home $CHAIN_HOME/node-a
+
+NODE_A_KEY=$(docker compose run --no-deps --rm node-a keys add validator-a --keyring-backend test --home $CHAIN_HOME/node-a --output json)
+NODE_B_KEY=$(docker compose run --no-deps --rm node-b keys add validator-b --keyring-backend test --home $CHAIN_HOME/node-b --output json)
+NODE_C_KEY=$(docker compose run --no-deps --rm node-c keys add validator-c --keyring-backend test --home $CHAIN_HOME/node-c --output json)
+NODE_D_KEY=$(docker compose run --no-deps --rm node-d keys add validator-d --keyring-backend test --home $CHAIN_HOME/node-d --output json)
+
+NODE_A_ADDRESS=$(echo $NODE_A_KEY | jq -r .address)
+NODE_B_ADDRESS=$(echo $NODE_B_KEY | jq -r .address)
+NODE_C_ADDRESS=$(echo $NODE_C_KEY | jq -r .address)
+NODE_D_ADDRESS=$(echo $NODE_D_KEY | jq -r .address)
+
+docker compose run --no-deps --rm node-a add-genesis-account $NODE_A_ADDRESS 10000000000000000000000000000aevmos --home $CHAIN_HOME/node-a
+docker compose run --no-deps --rm node-a add-genesis-account $NODE_B_ADDRESS 10000000000000000000000000000aevmos --home $CHAIN_HOME/node-a
+docker compose run --no-deps --rm node-a add-genesis-account $NODE_C_ADDRESS 10000000000000000000000000000aevmos --home $CHAIN_HOME/node-a
+docker compose run --no-deps --rm node-a add-genesis-account $NODE_D_ADDRESS 10000000000000000000000000000aevmos --home $CHAIN_HOME/node-a
 
 echo data/chain/node-b/config data/chain/node-c/config data/chain/node-d/config | xargs -n 1 cp data/chain/node-a/config/genesis.json
 
@@ -46,3 +53,9 @@ sed -i.bak 's/addr_book_strict = true/addr_book_strict = false/g' data/chain/nod
 sed -i.bak 's/addr_book_strict = true/addr_book_strict = false/g' data/chain/node-b/config/config.toml
 sed -i.bak 's/addr_book_strict = true/addr_book_strict = false/g' data/chain/node-c/config/config.toml
 sed -i.bak 's/addr_book_strict = true/addr_book_strict = false/g' data/chain/node-d/config/config.toml
+
+
+echo "Node A mnemonic:" $(echo $NODE_A_KEY | jq -r .mnemonic)
+echo "Node B mnemonic:" $(echo $NODE_B_KEY | jq -r .mnemonic)
+echo "Node C mnemonic:" $(echo $NODE_C_KEY | jq -r .mnemonic)
+echo "Node D mnemonic:" $(echo $NODE_D_KEY | jq -r .mnemonic)
